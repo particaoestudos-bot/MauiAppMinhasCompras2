c509d6e3cc5f5d1e9cc0be7fce295e2c3ae54b22
Tonybonilha <tony.vieira@etec.sp.gov.br>
2026-09-10 03:48:52 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras2/Helpers/SQliteDatabaseHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppMinhasCompras2.Helpers
+{
+    internal class SQliteDatabaseHelper
+    {
+    }
+}
---
+++ MauiAppMinhasCompras2/Views/NovoProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppMinhasCompras2.Views;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
